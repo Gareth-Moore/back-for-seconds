42e5f41e5683a249eea9f9d619839051d0833f33
Gareth-Moore <gccsmoore3@gmail.com>
2023-07-25 14:51:27 +0100

    added asyncHandler to userControllers

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,9 +1,11 @@
+import asyncHandler from "express-async-handler";
+
 // description:     Auth, user/set token
 // route:           POST api/users/auth
 // access:          Public
 
-const authUser = (req, res) => {
-  res.status(200).json({ message: "Autherise user" });
+const authUser = async (req, res) => {
+  res.status(200).json({ message: "Autherize user" });
 };
 
 // description:

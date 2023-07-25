def5d25295168caa03abc5e1cbd4c6402781dd3c
Gareth-Moore <gccsmoore3@gmail.com>
2023-07-25 16:13:21 +0100

    skeleton code for additional user routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,10 @@
 import asyncHandler from "express-async-handler";
 import { simulateError } from "../utils/errorUtils.js";
 
-// description:     Auth, user/set token
-// route:           POST api/users/auth
-// access:          Public
-
+// description:   Auth user / set token
+// method:        POST
+// route:         api/users/auth
+// access:        Public
 const authUser = asyncHandler(async (req, res) => {
   simulateError(false);
 
@@ -12,8 +12,50 @@ const authUser = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Autherize user" });
 });
 
-// description:
-// route:
-// access:
+// description:   Register a new user
+// method:        POST
+// route:         /api/users
+// access:        Public
+const registerUser = asyncHandler(async (req, res) => {
+  simulateError(false);
 
-export { authUser };
+  res.status(200).json({ message: "Register user" });
+});
+
+// description:   Logout User
+// method:        POST
+// route:         /api/users/logout
+// access:        Public
+const logoutUser = asyncHandler(async (req, res) => {
+  simulateError(false);
+
+  res.status(200).json({ message: "Logout user" });
+});
+
+// description:   Get users profile
+// method:        GET
+// route:         /api/users/profile
+// access:        Private
+const getUserProfile = asyncHandler(async (req, res) => {
+  simulateError(false);
+
+  res.status(200).json({ message: "Get user profile" });
+});
+
+// description:   Update user profile
+// method:        PUT
+// route:         /api/profile
+// access:        Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+  simulateError(false);
+
+  res.status(200).json({ message: "Update user profile" });
+});
+
+export {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+};

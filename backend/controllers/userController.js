803de9f50ca937a426cd695b6ce0e03d8b391aef
Gareth-Moore <gccsmoore3@gmail.com>
2023-07-27 14:38:01 +0100

    created user login and logout

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -9,9 +9,23 @@ import generateToken from "../utils/generateToken.js";
 // access:        Public
 const authUser = asyncHandler(async (req, res) => {
   simulateError(false);
+  console.log("Auth user");
 
-  console.log("start auth controller");
-  res.status(200).json({ message: "Autherize user" });
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    generateToken(res, user._id);
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid email or password");
+  }
 });
 
 // description:   Register a new user
@@ -56,8 +70,14 @@ const registerUser = asyncHandler(async (req, res) => {
 // access:        Public
 const logoutUser = asyncHandler(async (req, res) => {
   simulateError(false);
+  console.log("Logout user");
 
-  res.status(200).json({ message: "Logout user" });
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+
+  res.status(200).json({ message: "User logged out" });
 });
 
 // description:   Get users profile

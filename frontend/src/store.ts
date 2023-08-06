f05f40e5131d5ca20a96592c6933fc66779933e2
Gareth-Moore <gccsmoore3@gmail.com>
2023-08-06 09:48:27 +0100

    created auth reducer

--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "./slices/authSlice.ts";
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    auth: authReducer,
+  },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
   devTools: true,
 });

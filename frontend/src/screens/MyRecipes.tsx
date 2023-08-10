cbe63677c2e12e5daa2b82795c47b1b23a822296
Gareth-Moore <gccsmoore3@gmail.com>
2023-08-10 14:59:05 +0100

    cleaned up some unused code

--- frontend/src/screens/MyRecipes.tsx
+++ frontend/src/screens/MyRecipes.tsx
@@ -1,6 +1,4 @@
-import { Grid, Box } from "@chakra-ui/react";
 import SidebarGrid from "../components/SidebarGrid";
-import RecipeCard from "../components/RecipeCard";
 import RecipeCardCopy from "../components/RecipeCardCopy";
 
 const MyRecipes = () => {

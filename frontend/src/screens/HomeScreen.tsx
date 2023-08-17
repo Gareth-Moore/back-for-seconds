5bb1d6ab6d4ecde7e313fa0638df9f00f8f46d43
Gareth-Moore <gccsmoore3@gmail.com>
2023-08-17 09:24:36 +0100

    Updated home screen

--- frontend/src/screens/HomeScreen.tsx
+++ frontend/src/screens/HomeScreen.tsx
@@ -7,10 +7,14 @@ import {
   useBreakpointValue,
   Button,
   HStack,
+  Box,
 } from "@chakra-ui/react";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 const HomeScreen = () => {
+  const { userInfo } = useSelector((state: any) => state.auth);
+
   return (
     <Flex
       width="100%"
@@ -28,8 +32,9 @@ const HomeScreen = () => {
         height={"350px"}
         justify={"space-between"}
         borderRadius={10}
+        align={"flex-start"}
       >
-        <VStack padding={10} align={"flex-start"}>
+        <Box padding={10}>
           <Text
             fontFamily={"'Courier Prime', monospace"}
             textAlign={"left"}
@@ -39,40 +44,67 @@ const HomeScreen = () => {
           >
             Back for Seconds?
           </Text>
-          <Text
-            mt={5}
-            mb={8}
-            color={"gray.600"}
-            fontWeight={300}
-            fontSize={"lg"}
-            maxWidth={"400px"}
-          >
-            With all the recipes under the sun in an easy to find collection
-            you'll be cooking up a storm in no time!
-          </Text>
-          <Stack direction={"row"}>
+          <Box display={userInfo && userInfo.firstName ? "none" : "block"}>
+            <Text
+              mt={5}
+              mb={8}
+              color={"gray.600"}
+              fontWeight={300}
+              fontSize={"lg"}
+              maxWidth={"400px"}
+            >
+              With all the recipes under the sun in an easy to find collection
+              you'll be cooking up a storm in no time!
+            </Text>
+            <Stack direction={"row"} mt={"80px"}>
+              <Button
+                as={Link}
+                minWidth={"150px"}
+                bg={"red.400"}
+                color={"white"}
+                _hover={{ bg: "red.500" }}
+                to="/login"
+              >
+                Sign in
+              </Button>
+              <Button
+                as={Link}
+                minWidth={"150px"}
+                bg={"gray.300"}
+                color={"white"}
+                _hover={{ bg: "gray.500" }}
+                to={"/register"}
+              >
+                Register
+              </Button>
+            </Stack>
+          </Box>
+          <Box>
+            <Text
+              display={userInfo && userInfo.firstName ? "block" : "none"}
+              mt={5}
+              mb={8}
+              color={"gray.600"}
+              fontWeight={300}
+              fontSize={"lg"}
+              maxWidth={"400px"}
+            >
+              Welcome! Now that you're signed in, why not take a look around for
+              some delicious recipes!
+            </Text>
             <Button
+              mt={{ base: "10px", sm: "60px" }}
               as={Link}
-              minWidth={"150px"}
+              minWidth={"100%"}
               bg={"red.400"}
               color={"white"}
               _hover={{ bg: "red.500" }}
-              to="/login"
+              to="/explore-recipes"
             >
-              Sign in
+              Get started
             </Button>
-            <Button
-              as={Link}
-              minWidth={"150px"}
-              bg={"gray.300"}
-              color={"white"}
-              _hover={{ bg: "gray.500" }}
-              to={"/register"}
-            >
-              Register
-            </Button>
-          </Stack>
-        </VStack>
+          </Box>
+        </Box>
         <Image
           display={{ base: "none", md: "block" }}
           src="./src/assets/welcome.jpg"

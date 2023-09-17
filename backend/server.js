646ecba9abb7a63770925b85866efe77aa8c3d9e
Gareth-Moore <gccsmoore3@gmail.com>
2023-09-17 14:22:44 +0100

    uploaded dist app

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
 import commentRoutes from "./routes/commentRoutes.js";
@@ -43,7 +44,15 @@ app.use("/api/shopping-list", shoppingListRoutes);
 app.use("/api/image", imageRoutes);
 app.use("/api/recipes", recipeRoutes);
 
-app.get("/", (req, res) => res.send("Server is ready..."));
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, "frontend/dist")));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => res.send("Server is ready..."));
+}
 
 // error handlers
 app.use(routeNotFound);
